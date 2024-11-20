ae703e969ca18ddbaf4c7b33f9c79e2a92441e82
luciasm1 <luciasm1@TR12-201-50>
2024-11-20 12:46:10 +0100

    Agregar archivos de proyecto.

---
+++ inheritance.cpp
@@ -0,0 +1,11 @@
+// inheritance.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+}
+
